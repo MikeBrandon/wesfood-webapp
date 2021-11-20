95b6c5fe59fb3adb121b63b6fdb1bcb1100f8751
Mike Brandon <bkb.braock@gmail.com>
2021-11-20 23:09:23 +0300

    add: lottie

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,10 @@
     import Menu from '$lib/menu/index.svelte'
 </script>
 
+<svelte:head>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</svelte:head>
+
 <main>
     {#key $registered}
         {#if $registered == 0}
@@ -11,11 +15,48 @@
         {:else if $registered == 1}
             <Menu/>
         {:else}
-            Welcome to Wesfood!
+            <div>
+                <img src="https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg" alt="">
+                <h2>
+                    Welcome To Wesfood
+                </h2>
+                <p>
+                    Get food delivery to your doorstep from Wesfood. Find the meal you crave and order food easily with the wesfood web-app. Track your order in real-time.
+                </p>
+                <p>
+                    Click Login to get started
+                </p>
+                <lottie-player   lottie-player src="https://assets8.lottiefiles.com/packages/lf20_AzOvUp.json"  background="transparent"  speed="1"  style="width: 200px; height: 200px;" hover loop autoplay></lottie-player>
+            </div>
         {/if}
     {/key}
 </main>
 
 <style>
+    main {
+        color: white;
+        text-align: center;
+    }
 
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100vh;
+        position: relative;
+    }
+
+    h2 {
+        z-index: 10;
+        margin-top: 32px;
+        font-size: 32px;
+    }
+
+    img {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        object-fit: cover;
+        z-index: -10;
+    }
 </style>

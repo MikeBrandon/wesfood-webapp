3a3f8c3eafa0b70089583988474abd06fedd7375
Mike Brandon <bkb.braock@gmail.com>
2021-11-20 18:45:09 +0300

    add: admin route

---
+++ src/routes/admin.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { userStore } from '$lib/stores/authStore';
+    import AdminDashboard from '$lib/adminDashboard/index.svelte';
+
+    function userIsAdmin() {
+        return $userStore.email == 'bkb.braock@gmail.com';
+    }
+</script>
+
+<main>
+    {#if $userStore}
+        {#if userIsAdmin()}
+            <AdminDashboard userName={$userStore.displayName}/>
+        {:else}
+            <p>You are not an Admin!</p>
+        {/if}
+    {:else}
+        <p>Please Sign in!</p>
+    {/if}
+</main>
+
+<style>
+
+</style>
